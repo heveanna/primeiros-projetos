e5c105b0d7de826d757307b6316e3b40803e5302
Anna Hevellyn <annahevellynn@gmail.com>
2025-06-18 14:56:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-# FAZER UM MINI SISTEMA 
+# OO E MÉTODO CONSTRUTOR
 
-class Pessoass:
-    def __int__(self, nome, idade):
-        self.nome = nome
+class Pessoa:
+    ''' classe pessoaas'''
+    def __int__(self, idade):
         self.idade = idade
-        self.pessoas = []
-        
+        print ('A idade da pessoa é', self.idade)
 
+idade_pessoa = input('Digite a sua idade: ')
+p1 = Pessoa (idade_pessoa)
+
+class Gato:
+    def __init__ (self, nome):
+        self.nome = nome
+        print('Seu gato se chama', self.nome)
+
+nome_gato = input('Digite o nome do seu gato: ')
+g1 = Gato (nome_gato)
