b4cf15751274aa4ff81d8837b88c1aace79720cb
heveanna <annahevellynn@gmail.com>
2025-11-05 10:06:02 -0300

    ss

--- py code/leve.py
+++ py code/leve.py
@@ -1,9 +1,14 @@
 
 
 class Pessoa:
-    def __init__(self, idade, altura):
+    def __init__(self, idade, altura, nome):
         self.idade = idade
         self.__altura = altura
-        
+        self.nome = nome
+
+    def peso_pesso(self, peso):
+        self.peso = peso
+
+
         
         
