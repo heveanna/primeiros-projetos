98f592a4f34f36419a5e3fccec58dbf181d0a30f
heveanna <annahevellynn@gmail.com>
2025-11-12 21:25:37 -0300

    Att

--- py code/leve.py
+++ py code/leve.py
@@ -1,15 +1,15 @@
 import pandas as pd 
+import numpy as np
 
 class Pessoa:
-    def __init__(self, idade, altura, nome):
+    def __init__(self, idade, altura, nome, sobrenome):
         self.idade = idade
         self.__altura = altura
         self.nome = nome
+        self.sobrenome = sobrenome
 
     def peso_pesso(self, peso, cabelo):
         self.peso = peso
         self.cabelo = cabelo
 
-
-        
         
